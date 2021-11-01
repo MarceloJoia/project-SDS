a0ddbc7d511baabfceecb2d34e7969e8de92bb0a
MarceloJoia <sitejoia@hotmail.com>
2021-11-01 15:45:33 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/MarceloJoia/" target="_blank" rel="noreferrer">Marcelo Joia</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/joiamarketing/" target="_blank" rel="noreferrer">@joiamarketing</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
