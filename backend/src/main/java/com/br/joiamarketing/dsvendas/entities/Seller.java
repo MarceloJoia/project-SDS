67a0a92afe29d90b1e6cbc33a1482263877762cd
MarceloJoia <sitejoia@hotmail.com>
2021-11-03 11:50:38 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/br/joiamarketing/dsvendas/entities/Seller.java
@@ -0,0 +1,54 @@
+package com.br.joiamarketing.dsvendas.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+	
+	@Id //Define Primary Key is ID
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Auto Increment
+	private Long id;
+	private String name;
+	
+	//Foreign Key One to Many -> 1 > * 
+	@OneToMany(mappedBy = "seller") 
+	private List<Sale>sales = new ArrayList<>();
+	
+	public Seller() {}
+
+	public Seller(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Sale> getSales() {
+		return sales;
+	}
+	
+	
+}
