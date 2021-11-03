3d64ea570b23d6ae8d53078ec310bc406793fedb
MarceloJoia <sitejoia@hotmail.com>
2021-11-03 15:06:58 -0300

    Layers

---
+++ backend/src/main/java/com/br/joiamarketing/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.br.joiamarketing.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.joiamarketing.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
---
+++ backend/src/main/java/com/br/joiamarketing/dsvendas/service/SellerService.java
@@ -0,0 +1,23 @@
+package com.br.joiamarketing.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.joiamarketing.dsvendas.dto.SellerDTO;
+import com.br.joiamarketing.dsvendas.entities.Seller;
+import com.br.joiamarketing.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result =  repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
